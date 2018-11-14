7eb104fafccb1c9bc1536ed2e93c32ecbbceac31
smdimagerie <simon@imagerie.ca>
2018-11-14 18:34:38 -0500

    Create Anchor.php

---
+++ Anchor.php
@@ -0,0 +1,20 @@
+<?php
+
+/*
+ * (c) Balise.ca
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+// If you don't to add a custom vendor folder, then use the simple class
+// namespace HelloComposer;
+namespace Balise\AnchorFramework;
+
+class Anchor
+{
+    public function render()
+    {
+        return "what";
+    }
+}
