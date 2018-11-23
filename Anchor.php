142d3d5aca1554ffa5cf0015b5582d8316838c87
Unknown <simon@imagerie.ca>
2018-11-23 13:42:32 -0500

    Improved Template querying
    There's still work in progress

--- Anchor.php
+++ Anchor.php
@@ -33,19 +33,46 @@ class Anchor
         return $return;
     }
     private static function getTemplate() {
-        if (is_404()) { return array('404','index'); }
-        if (is_search()) { return array('search','index'); }
-        if (is_singular()) {
-            if (is_page()) {
-                return array('page','index');
-            } elseif(is_single()) {
-                return array('single-post','single','index');
-            } elseif (is_attachment()) {
-                return array('attachment','index');
-            }
 
+        global $post;
+
+        $templates = array('index');
+        if (is_404()) { array_unshift($templates, '404'); }
+        if (is_search()) { array_unshift($templates, 'search'); }
+
+        if (is_singular()) { array_unshift($templates, 'singular'); }
+
+        if (is_attachment()) {
+            $mime = $post->post_mime_type;
+            array_unshift($templates, $template, $mime, 'attachment');
+            // TOTO: COMPLETE WITH MEDIA
+
+        } elseif (is_single()) {
+            $type = $post->post_type;
+            $slug = $post->post_name;
+            $id = $post->id;
+            $template = get_page_template();
+            array_unshift($templates, $template, 'single-'.$slug.'-'.$type, 'single-'.$type,'single');
         }
-        return array('index');
+        if (is_page()) {
+            $slug = $post->post_name;
+            $id = $post->id;
+            $template = get_page_template();
+            array_unshift($templates, $template, 'page-'.$slug, 'page-'.$id,'page');
+        }
+
+
+        if (is_archive()) { array_unshift($templates, 'archive'); }
+        if (is_tag()) {
+            $queried = get_queried_object();
+            array_unshift($templates, 'tag-'.$queried->rewrite['slug'],'tag-'.$queried->term_id, 'tag');
+        }
+        // TODO; COMPLETE ARCHIVE
+
+        if (is_home()) { array_unshift($templates, 'home'); }
+        if (is_front_page()) { array_unshift($templates, 'front-page'); }
+
+        return ;
     }
 
     private static function loadTemplate($array) {
