614cefd0a2e149b09a9e400c99f39af5296c5fd3
Simon Mongeau <simon@imagerie.ca>
2019-02-01 12:31:52 -0500

    Update TaxonomyWrapper.php

--- src/TaxonomyWrapper.php
+++ src/TaxonomyWrapper.php
@@ -1,56 +1,66 @@
 <?php
 /*
-* (c) Balise.ca
-*
-* This source file is subject to the MIT license that is bundled
-* with this source code in the file LICENSE.
-*/
+ * (c) Balise.ca
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
 namespace Balise\AnchorFramework;
-class TaxonomyWrapper {
+
+class TaxonomyWrapper
+{
     private $isSync;
-    function __construct($taxonomy, $post=null,$isSync=false) {
+    public function __construct($taxonomy, $post = null, $isSync = false)
+    {
         $this->isSync = $isSync;
-        if (gettype($taxonomy)==="string") {
+        if (gettype($taxonomy) === "string") {
             $taxonomy = get_taxonomy($taxonomy);
         }
-        if ($taxonomy && gettype($taxonomy)==="object") {
+        if ($taxonomy && gettype($taxonomy) === "object") {
             $this->slug = $taxonomy->name;
-            $this->title = $taxonomy->labels->name; 
+            $this->title = $taxonomy->labels->name;
             $this->content = apply_filters('the_content', $taxonomy->description);
-            $this->excerpt = wp_trim_words($taxonomy->description,  apply_filters( 'excerpt_length', 55 ), ' ' . '[&hellip;]');
+            $this->excerpt = wp_trim_words($taxonomy->description, apply_filters('excerpt_length', 55), ' ' . '[&hellip;]');
 
             if ($post) {
                 $this->terms = get_the_terms($post, $taxonomy->name);
             } else {
-                $this->terms = get_terms( array(
+                $this->terms = get_terms(array(
                     'taxonomy' => $taxonomy->name,
                     'hide_empty' => false,
-                ) );
+                ));
             }
-            $this->terms = array_map(function($item) {
-                return new TermWrapper($item, true);
-            }, $this->terms);
+            if ($this->terms) {
+                $this->terms = array_map(function ($item) {
+                    return new TermWrapper($item, true);
+                }, $this->terms);
+            } else {
+                $this->terms = array();
+            }
         }
         $this->isSync = false;
     }
 }
 
-
 /*
-* LOAD THE POST ON DEMAND
-*/
-class AsyncTaxonomyWrapper {
+ * LOAD THE POST ON DEMAND
+ */
+class AsyncTaxonomyWrapper
+{
     private $virtual;
-    function __construct($id) {
+    public function __construct($id)
+    {
         $this->id = $id;
         $this->virtual = null;
     }
-    public function __tostring() {
+    public function __tostring()
+    {
         return $this->id;
     }
-    public function __get($name) {
+    public function __get($name)
+    {
         if (!$this->virtual) {
-             $this->virtual = new TaxonomyWrapper($this->id);
+            $this->virtual = new TaxonomyWrapper($this->id);
         }
         if ($this->virtual) {
             return $this->virtual->$name;
