a42f9c0f6ca1d262454985d0192c7a1c36ed8be3
Simon Mongeau <simon@imagerie.ca>
2019-10-18 11:32:36 -0400

    Create wrapper.blade.php

---
+++ views/wp/wrapper.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html <?php language_attributes() ?>>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <?php wp_head(); ?>
+</head> 
+<body <?php body_class(); ?>>
+    @yield('body')
+    <?php wp_footer(); ?>
+</body>
+</html>
