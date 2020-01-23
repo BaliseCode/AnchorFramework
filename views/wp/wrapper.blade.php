9dec9109ba5bafe99791f4777e19eb0fe2b2dbbd
Simon Mongeau <simon@imagerie.ca>
2020-01-23 14:29:43 -0500

    Update wrapper.blade.php

--- views/wp/wrapper.blade.php
+++ views/wp/wrapper.blade.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <?php wp_head(); ?>
+    @yield('head')
 </head> 
 <body <?php body_class(); ?>>
     @yield('body')
